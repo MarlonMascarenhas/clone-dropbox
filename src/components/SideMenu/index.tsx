969e6659b508e69e0fbf84c31b829c642c6ab133
Marlon Mascarenhas <mmascarenhs@gmail.com>
2021-02-26 15:10:11 -0300

    estilizacao da SideMenu

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,11 +1,35 @@
-import React, { Children } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Container } from './styles';
 
-const SideMenu: React.FC = () => {
+const scrollThreshold = 300;
+
+const SideMenu: React.FC = ({ children }) => {
+    const [scrollY, setScrollY] = useState(0);
+    const [isActive, setIsActive] = useState(false);
+
+
+    useEffect(() => {
+        function onScroll() {
+            setScrollY(window.scrollY);
+            setIsActive(false);
+        }
+        window.addEventListener('scroll', onScroll)
+
+        return() => window.removeEventListener('scroll', onScroll);
+    }, []);
+
+    const classes = [
+        isActive ? 'open': '',
+        scrollY <= scrollThreshold ? 'scrollOpen' : '',
+    ]
+
+    const className = classes.join(' ').trim();
+
+
     return (
-        <Container>
-            {Children}
+        <Container className={className}>
+            {children}
         </Container>
     );
 }
